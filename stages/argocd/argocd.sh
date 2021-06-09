f5d0a282fb02fbe493ebe23755dde97c19cfbc19
Chehine <chehine_marouani@hotmail.com>
2021-06-09 08:26:21 -0400

    Update argocd.sh

--- stages/argocd/argocd.sh
+++ stages/argocd/argocd.sh
@@ -2,7 +2,7 @@
 
 argocd login 34.152.23.230:30796 --grpc-web --username admin --password $ARGOCD_PASSWORD --insecure
 
-argocd app get maven-project-$ENV
+argocd app set maven-project-$ENV --kustomize-images $DOCKER_HUB_ACCOUNT/maven-project-$ENV:$BUILD_TAG
 argocd app sync maven-project-$ENV --force --prune
 
 argocd logout 34.152.23.230:30796
